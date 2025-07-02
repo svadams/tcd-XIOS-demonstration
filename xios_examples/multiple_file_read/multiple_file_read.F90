e760850667426985702a93df308ed6b816afab7e
Samantha Adams <samantha.adams@metoffice.gov.uk>
2025-07-02 14:54:38 +0100

    Adding a file writing context to combine the inputs. Working.

--- xios_examples/multiple_file_read/multiple_file_read.F90
+++ xios_examples/multiple_file_read/multiple_file_read.F90
@@ -31,6 +31,7 @@ contains
 
 
     ! Arbitrary datetime setup, required for XIOS but unused
+    ! TODO - should this be in the file or in the model code?
     origin = xios_date(2022, 2, 2, 12, 0, 0)
     start = xios_date(2022, 12, 13, 12, 0, 0)
     tstep = xios_hour
@@ -82,6 +83,17 @@ contains
     call xios_set_domain_attr("input_domain_2", ni=lenx, nj=leny, ibegin=0, jbegin=0)
     
     call xios_close_context_definition()
+    
+    ! initialize context for writing output data
+    call xios_context_initialize('output', comm)
+
+    call xios_set_time_origin(origin)
+    call xios_set_start_date(start)
+    call xios_set_timestep(tstep)
+
+    call xios_set_domain_attr("output_domain", ni=lenx, nj=leny, ni_glo=lenx, nj_glo=leny, ibegin=0, jbegin=0)
+    
+    call xios_close_context_definition()
        
 
   end subroutine initialise
@@ -89,14 +101,6 @@ contains
   subroutine finalise()
 
     integer :: mpi_error
-
-    ! Finalise all XIOS contexts and MPI
-    !call xios_set_current_context('domain_check')
-    !call xios_context_finalize()
-    !call xios_set_current_context('input_1')
-    !call xios_context_finalize()
-    !call xios_set_current_context('input_2')
-    !call xios_context_finalize()
     call MPI_Comm_free(comm, mpi_error)
     call xios_finalize()
     call MPI_Finalize(mpi_error)
@@ -113,6 +117,7 @@ contains
     ! Allocatable arrays, size is taken from input file
     double precision, dimension (:,:), allocatable :: field_A
     double precision, dimension (:,:), allocatable :: field_B
+    double precision, dimension (:,:), allocatable :: field_C
     
     ! Switch to context for first read
     
@@ -148,18 +153,37 @@ contains
     
     ! We don't need this read context now so finalize
     call xios_context_finalize()
+    
+    ! Switch to context for write
+    
+    call xios_set_current_context('output')
 
-    do ts=1, 1
+    call xios_get_domain_attr('output_domain', ni_glo=lenx)
+    call xios_get_domain_attr('output_domain', nj_glo=leny)
+    
+    print *, 'output domain x, output domain y', lenx, ', ', leny
+    
+    allocate ( field_C(leny, lenx) )
+    
+    ! initialise field_C
+    
+    field_C = field_A + field_B
+
+    do ts=1, 5
       call xios_update_calendar(ts)
       call xios_get_current_date(current)
-      ! Send (copy) the original data to the output file.
-      !call xios_send_field('odata', inodata)
-      ! Send (copy) the expected data to the output file.
-      !call xios_send_field('edata', inedata)
+      ! Change our data so we can see clearly what is happening
+      field_C(:,:) = field_C(:,:) + 1.0d0
+      ! Send data to the output file.
+      call xios_send_field('field_C', field_C)
     enddo
+    
+    ! We don't need this write context now so finalize
+    call xios_context_finalize()
 
     deallocate (field_A)
     deallocate (field_B)
+    deallocate (field_C)
 
   end subroutine simulate
 
